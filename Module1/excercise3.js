29811e7b4f26c87000d43f802b9bd871960b3592
rbimal49 <rbimal49@gmail.com>
2024-02-26 19:53:34 +1300

    background change code added

--- Module1/excercise3.js
+++ Module1/excercise3.js
@@ -1,21 +1,13 @@
-function helloWorld() {
-  alert("Hey Wass up Man!");
-  document.getElementById("demo").style.color = "red";
-}
-
 //color change on click
 
 //function to change color of "Hello"
 function hello() {
-  document.getElementById("heading1").style.color = "red";
+  document.getElementById("heading1").style.color = "white";
+  document.getElementById("column1").style.backgroundColor = "grey";
 }
 
 //function to change color of "World"
 function world() {
-  document.getElementById("heading2").style.color = "blue";
-}
-
-function calculation(a, b, c) {
-  if (a == b) console.warn("a must not be equal to b");
-  if (b == c) console.error("b must nerver be equal to c");
+  document.getElementById("heading2").style.color = "green";
+  document.getElementById("column2").style.backgroundColor = "skyblue";
 }
