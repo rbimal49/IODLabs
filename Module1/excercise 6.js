33c9e764333e6a0b5871f0dde197c9e123c43f72
rbimal49 <rbimal49@gmail.com>
2024-03-09 09:17:26 +1300

    excercise6 completed

---
+++ Module1/excercise 6.js
@@ -0,0 +1,13 @@
+let book = {
+  title: "ABC",
+  description: "Good Book",
+  author: "Mr. Rai",
+  number_of_pages: 50,
+};
+
+console.log(book["title"]);
+console.log(book["description"]);
+console.log(book["author"]);
+console.log(book["number_of_pages"]);
+
+book.description = "Best Book!!";
