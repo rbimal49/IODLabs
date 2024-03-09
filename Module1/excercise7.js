021fe90976734a8e6e4c7d67ea332d945db98d7b
rbimal49 <rbimal49@gmail.com>
2024-03-09 15:15:10 +1300

    Updated Module1 Excercise7 with unit test

--- Module1/excercise7.js
+++ Module1/excercise7.js
@@ -4,18 +4,18 @@ function add(a, b) {
 }
 
 // Unit test for the add function
-console.log(add(6, 3)); // Expected total:9 / Result:9
-console.log(add(6, -9)); // Expected negative number: -3 / Result: -3
-console.log(add(0, 0)); // Expected number: 0 / Result: 0
+console.log(add(6, 3) == 9); // Expected result on console: True
+console.log(add(6, -9) == 3); // Expected result on console: False
+console.log(add(0, 0) == 0); // Expected result on console: True
 
 // function to subtract two numbers and returns the result.
 function subtract(a, b) {
   return a - b;
 }
 //Unit test for the subtract fuchtion
-console.log(subtract(6, 3)); //Expected reuslt: 3 / Result: 3
-console.log(subtract(6, 20)); // Expected negative number: -14 / Result: -14
-console.log(subtract(0, 0)); // Expected number: 0 / Result: 0
+console.log(subtract(6, 3) == 3); // Expected result on console: True
+console.log(subtract(6, 20) == -14); // Expected result on console: True
+console.log(subtract(6, 6) == 0); // Expected result on console: True
 
 // function to multiply two numbers and returns the result.
 function multiply(a, b) {
@@ -23,9 +23,9 @@ function multiply(a, b) {
 }
 
 //Unit test for the multiply fuchtion
-console.log(multiply(6, 3)); //Expected reuslt: 18 / Result: 18
-console.log(multiply(6, -2)); // Expected negative number: -12 / Result: -12
-console.log(multiply(6, 0)); // Expected number: 0 / Result: 0
+console.log(multiply(6, 3) == 19); // Expected result on console: False
+console.log(multiply(6, -2) == -12); // Expected result on console: True
+console.log(multiply(6, 0) == 0); // Expected result on console: True
 
 // function to divide two numbers
 function divide(a, b) {
@@ -33,17 +33,16 @@ function divide(a, b) {
 }
 
 //Unit test for the divide fuchtion
-console.log(divide(6, 3)); //Expected reuslt: 2 / Result: 2
-console.log(divide(6, -2)); // Expected negative number: -3 / Result: -3
-console.log(divide(6, 0)); // Expected number: infinity / Result: infinity
+console.log(divide(6, 3) == 2); // Expected result on console: True
+console.log(divide(6, -2) == -2); // Expected result on console: True
+console.log(divide(0, 6) == 0); // Expected result on console: True
 
 // function that takes the name of person and prints "Hello <name>"
-
 function greet(name) {
-  console.log("Hello ".concat("", name));
+  return `Hello ${name}`;
 }
 
 //Unit test for the greet function
-greet("Joe"); //Expected result: Hello Joe / Result: Hello Joe
-greet(""); //Expected result: Hello / Result: Hello
-greet("Jill"); ///Expected result: Hello Jill / Result: Hello Jill
+console.log(greet("Joe") == "Hello Joe"); // Expected result on console: True
+console.log(greet("World") == "Hello"); // Expected result on console: False
+console.log(greet("Jill") == "Hello Joe"); // Expected result on console: False
