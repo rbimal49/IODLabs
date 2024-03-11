814b5113a3bc85c3cd73a7b6cd9154e2dd982011
rbimal49 <rbimal49@gmail.com>
2024-03-11 21:01:06 +1300

    Dice hidden in default state

---
+++ Module3/objectType.js
@@ -0,0 +1,12 @@
+let student = {
+  firstName: "Bimal",
+  lastName: "Rai",
+  startDate: "14 Feb 2024",
+  studyCourse: "Software Developer",
+};
+
+console.log(student);
+console.log(student.firstName);
+console.log(student.lastName);
+console.log(student.startDate);
+console.log(student.studyCourse);
