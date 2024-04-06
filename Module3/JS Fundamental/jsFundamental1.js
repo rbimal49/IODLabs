5d9893e04fbde2528699972fc84f584195e9ce47
rbimal49 <rbimal49@gmail.com>
2024-04-06 13:26:53 +1300

    Js Fundamental1 completed

---
+++ Module3/JS Fundamental/jsFundamental1.js
@@ -0,0 +1,36 @@
+// 1. What are the results of these expressions? (answer first, then use console.log() to check)
+"" + 1 + 0; //      will result 10.
+"" - 1 + 0; //      will result -10.
+true + false; //    true and false are treated as 1 and 0 respectively so the result will be 1.
+!true; //           is not true, so result will be false1
+6 / "3"; //         result will be 2
+"2" * "3"; //       result will be 6
+4 + 5 + "px"; //    result will be 9px
+"$" + 4 + 5; //     result will be $45
+"4" - 2; //         result will be 2
+"4px" - 2; //       result will be nan
+" -9 " + 5; //      result will be -95
+" -9 " - 5; //      result will be -14
+null + 1; //        result will be 1
+undefined + 1; //   reuslt will be NaN
+undefined == null; //result will be true
+undefined === null; //result will be false
+" \t \n" - 2; //    \t \n will be converted to 0 so the result will be false
+// prindint in console
+console.log("" + 1 + 0);
+console.log("" - 1 + 0);
+console.log(true + false);
+console.log(!true);
+console.log(6 / "3");
+console.log("2" * "3");
+console.log(4 + 5 + "px");
+console.log("$" + 4 + 5);
+console.log("4" - 2);
+console.log("4px" - 2);
+console.log("-9" + 5);
+console.log("-9" - 5);
+console.log(null + 1);
+console.log(undefined + 1);
+console.log(undefined == null);
+console.log(undefined === null);
+console.log(" \t \n" - 2);
