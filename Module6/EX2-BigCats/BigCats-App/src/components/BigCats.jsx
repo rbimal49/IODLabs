109df079c6fcc7c28d06e9c90265d8010bf7da41
Bimal Rai <rbimal49@gmail.com>
2024-05-16 19:50:22 +1200

    Ex-2 BigCats removed form EX1

--- Module6/EX2-BigCats/BigCats-App/src/components/BigCats.jsx
+++ Module6/EX2-BigCats/BigCats-App/src/components/BigCats.jsx
@@ -2,43 +2,43 @@ import React from "react";
 
 const cats = [
   {
-    id: "1",
+    id: "1001",
     name: "Cheetah",
     latinName: "Acinonyx jubatus",
     Image: "../public/Cheetah.jpg",
   },
   {
-    id: "2",
+    id: "1002",
     name: "Cougar",
     latinName: "Puma concolor",
     Image: "../public/Cougar.jpg",
   },
   {
-    id: "3",
+    id: "1003",
     name: "Jaguar",
     latinName: "Panthera onca",
     Image: "../public/Jaguar.jpg",
   },
   {
-    id: "4",
+    id: "1004",
     name: "Leopard",
     latinName: "Panthera pardus",
     Image: "../public/Leopard.jpg",
   },
   {
-    id: "5",
+    id: "1005",
     name: "Lion",
     latinName: "Panthera leo",
     Image: "../public/Lion.jpg",
   },
   {
-    id: "6",
+    id: "1006",
     name: "Snow leopard",
     latinName: "Panthera uncia",
     Image: "../public/Snow Leopard.jpg",
   },
   {
-    id: "7",
+    id: "1007",
     name: "Tiger",
     latinName: "Panthera tigris",
     Image: "../public/Tiger.jpg",
@@ -54,7 +54,8 @@ const BigCats = () => {
           <li key={cat.id}>
             {/* ID: {cat.id} - <storng>{cat.name}</storng> - {cat.latinName} */}
             <p>
-              {cat.id} - Name: {cat.name} - - - - - -Latin Name: {cat.latinName}
+              ID: {cat.id} - Name: {cat.name} - - - - - -Latin Name:{" "}
+              {cat.latinName}
             </p>
             <img src={cat.Image} className="image-container" alt="cat.name" />
           </li>
