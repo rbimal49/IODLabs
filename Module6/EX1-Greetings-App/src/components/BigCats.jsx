2b6e4833384fec3525d8c416c34b0839d7e6bd1e
Bimal Rai <rbimal49@gmail.com>
2024-05-16 18:57:25 +1200

    image added on exercise2

--- Module6/EX1-Greetings-App/src/components/BigCats.jsx
+++ Module6/EX1-Greetings-App/src/components/BigCats.jsx
@@ -1,13 +1,48 @@
 import React from "react";
 
 const cats = [
-  { id: "1", name: "Cheetah", latinName: "Acinonyx jubatus" },
-  { id: "2", name: "Cougar", latinName: "Puma concolor" },
-  { id: "3", name: "Jaguar", latinName: "Panthera onca" },
-  { id: "4", name: "Leopard", latinName: "Panthera pardus" },
-  { id: "5", name: "Lion", latinName: "Panthera leo" },
-  { id: "6", name: "Snow leopard", latinName: "Panthera uncia" },
-  { id: "7", name: "Tiger", latinName: "Panthera tigris" },
+  {
+    id: "1",
+    name: "Cheetah",
+    latinName: "Acinonyx jubatus",
+    Image: "../public/Cheetah.jpg",
+  },
+  {
+    id: "2",
+    name: "Cougar",
+    latinName: "Puma concolor",
+    Image: "../public/Cougar.jpg",
+  },
+  {
+    id: "3",
+    name: "Jaguar",
+    latinName: "Panthera onca",
+    Image: "../public/Jaguar.jpg",
+  },
+  {
+    id: "4",
+    name: "Leopard",
+    latinName: "Panthera pardus",
+    Image: "../public/Leopard.jpg",
+  },
+  {
+    id: "5",
+    name: "Lion",
+    latinName: "Panthera leo",
+    Image: "../public/Lion.jpg",
+  },
+  {
+    id: "6",
+    name: "Snow leopard",
+    latinName: "Panthera uncia",
+    Image: "../public/Snow Leopard.jpg",
+  },
+  {
+    id: "7",
+    name: "Tiger",
+    latinName: "Panthera tigris",
+    Image: "../public/Tiger.jpg",
+  },
 ];
 
 const BigCats = () => {
@@ -19,8 +54,9 @@ const BigCats = () => {
           <li key={cat.id}>
             {/* ID: {cat.id} - <storng>{cat.name}</storng> - {cat.latinName} */}
             <p>
-              {cat.id} - Name: {cat.name} - - - - - -Latin Name:{cat.latinName}
+              {cat.id} - Name: {cat.name} - - - - - -Latin Name: {cat.latinName}
             </p>
+            <img src={cat.Image} className="image-container" alt="cat.name" />
           </li>
         ))}
       </ul>
